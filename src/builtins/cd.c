8139876cb80f88eaf18d29680810d021d4e1b1c3
Pukiediederik <diederik.boeren@gmail.com>
2022-08-20 16:45:34 +0100

    Fixed issues with PWD & OLDPWD

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -17,23 +17,29 @@
 static int change_dir(char *path)
 {
 	char cwd[1024];
+	char cwd_new[1024];
 	char	*old_env;
 	char	*new_env;
 
 	if(!getcwd(cwd, 1024))
 		print_error("cd", "Problems getting cwd");
 	else if (!chdir(path)){
-		old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
-		new_env = ft_calloc(ft_strlen(path) + 5, sizeof(char));
-		ft_memcpy(old_env, "OLDPWD=", 7);
-		ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
-		ft_memcpy(new_env, "PWD=", 4);
-		ft_memcpy(new_env + 4, path, ft_strlen(cwd));
-		add_env(old_env);
-		add_env(new_env);
-		free(old_env);
-		free(new_env);
-		return (0);
+		if (!getcwd(cwd_new, 1024))
+			print_error("cd", "Could not update cwd");
+		else
+		{
+			old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
+			new_env = ft_calloc(ft_strlen(cwd_new) + 5, sizeof(char));
+			ft_memcpy(old_env, "OLDPWD=", 7);
+			ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
+			ft_memcpy(new_env, "PWD=", 4);
+			ft_memcpy(new_env + 4, cwd_new, ft_strlen(cwd_new));
+			add_env(old_env);
+			add_env(new_env);
+			free(old_env);
+			free(new_env);
+			return (0);
+		}
 	}
 	print_error("cd", "Could not change dir");
 	return (1);
@@ -42,6 +48,7 @@ static int change_dir(char *path)
 static int old_path(void)
 {
 	char cwd[1024];
+	char cwd_new[1024];
 	char	*old;
 	char	*old_env;
 	char	*new_env;
@@ -49,18 +56,23 @@ static int old_path(void)
 	old = get_env("OLDPWD");
 	if(!getcwd(cwd, 1024) || !old)
 		print_error("cd", "Problems getting cwd or $OLDPWD");
-	else if (!chdir(old)){
-		old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
-		new_env = ft_calloc(ft_strlen(old) + 5, sizeof(char));
-		ft_memcpy(old_env, "OLDPWD=", 7);
-		ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
-		ft_memcpy(new_env, "PWD=", 4);
-		ft_memcpy(new_env + 4, old, ft_strlen(cwd));
-		add_env(old_env);
-		add_env(new_env);
-		free(old_env);
-		free(new_env);
-		return (0);
+	else if (!chdir(old))
+	{
+		if (!getcwd(cwd_new, 1024))
+			print_error("cd", "Could not update cwd");
+		else {
+			old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
+			new_env = ft_calloc(ft_strlen(cwd_new) + 5, sizeof(char));
+			ft_memcpy(old_env, "OLDPWD=", 7);
+			ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
+			ft_memcpy(new_env, "PWD=", 4);
+			ft_memcpy(new_env + 4, cwd_new, ft_strlen(cwd_new));
+			add_env(old_env);
+			add_env(new_env);
+			free(old_env);
+			free(new_env);
+			return (0);
+		}
 	}
 	print_error("cd", "Could not change dir");
 	return (1);
