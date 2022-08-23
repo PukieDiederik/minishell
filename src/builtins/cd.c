4e82becbb89b96f4d7e0c7e2da0a7142b79e3434
Pukiediederik <diederik.boeren@gmail.com>
2022-08-23 23:13:03 +0100

    NORM cd.c

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: galpers <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: drobert- <drobert-@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/26 15:35:47 by galpers           #+#    #+#             */
-/*   Updated: 2022/07/26 15:37:31 by galpers          ###   ########.fr       */
+/*   Created: 2022/08/23 23:05:05 by drobert-          #+#    #+#             */
+/*   Updated: 2022/08/23 23:05:07 by drobert-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,46 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static int change_dir(char *path)
+static int	update_envs(char *old, char *new)
 {
-	char cwd[1024];
-	char cwd_new[1024];
 	char	*old_env;
 	char	*new_env;
+	int		return_s;
 
-	if(!getcwd(cwd, 1024))
+	return_s = 0;
+	old_env = ft_calloc(ft_strlen(old) + 8, sizeof(char));
+	new_env = ft_calloc(ft_strlen(new) + 5, sizeof(char));
+	if (!old_env || !new_env)
+	{
+		free(old_env);
+		free(new_env);
+		return (1);
+	}
+	ft_memcpy(old_env, "OLDPWD=", 7);
+	ft_memcpy(old_env + 7, old, ft_strlen(old));
+	ft_memcpy(new_env, "PWD=", 4);
+	ft_memcpy(new_env + 4, new, ft_strlen(new));
+	if (add_env(old_env) || add_env(new_env))
+		return_s = 1;
+	free(old_env);
+	free(new_env);
+	return (return_s);
+}
+
+static int	change_dir(char *path)
+{
+	char	cwd[1024];
+	char	cwd_new[1024];
+
+	if (!getcwd(cwd, 1024))
 		print_error("cd", "Problems getting cwd");
-	else if (!chdir(path)){
+	else if (!chdir(path))
+	{
 		if (!getcwd(cwd_new, 1024))
 			print_error("cd", "Could not update cwd");
 		else
 		{
-			old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
-			new_env = ft_calloc(ft_strlen(cwd_new) + 5, sizeof(char));
-			ft_memcpy(old_env, "OLDPWD=", 7);
-			ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
-			ft_memcpy(new_env, "PWD=", 4);
-			ft_memcpy(new_env + 4, cwd_new, ft_strlen(cwd_new));
-			add_env(old_env);
-			add_env(new_env);
-			free(old_env);
-			free(new_env);
+			update_envs(cwd, cwd_new);
 			return (0);
 		}
 	}
@@ -46,33 +62,23 @@ static int change_dir(char *path)
 	return (1);
 }
 
-static int old_path(void)
+static int	old_path(void)
 {
-	char cwd[1024];
-	char cwd_new[1024];
+	char	cwd[1024];
+	char	cwd_new[1024];
 	char	*old;
-	char	*old_env;
-	char	*new_env;
 
 	old = get_env("OLDPWD");
-	if(!getcwd(cwd, 1024) || !old)
+	if (!getcwd(cwd, 1024) || !old)
 		print_error("cd", "Problems getting cwd or $OLDPWD");
 	else if (!chdir(old))
 	{
 		if (!getcwd(cwd_new, 1024))
 			print_error("cd", "Could not update cwd");
-		else {
+		else
+		{
 			printf("%s\n", cwd_new);
-			old_env = ft_calloc(ft_strlen(cwd) + 8, sizeof(char));
-			new_env = ft_calloc(ft_strlen(cwd_new) + 5, sizeof(char));
-			ft_memcpy(old_env, "OLDPWD=", 7);
-			ft_memcpy(old_env + 7, cwd, ft_strlen(cwd));
-			ft_memcpy(new_env, "PWD=", 4);
-			ft_memcpy(new_env + 4, cwd_new, ft_strlen(cwd_new));
-			add_env(old_env);
-			add_env(new_env);
-			free(old_env);
-			free(new_env);
+			update_envs(cwd, cwd_new);
 			return (0);
 		}
 	}
