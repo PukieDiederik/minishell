0dda6faa9406b27eabfec9b18e941a0fe8b3f50e
Pukiediederik <diederik.boeren@gmail.com>
2022-09-08 02:53:22 +0100

    Norm get_program_path.c

--- src/get_program_path.c
+++ src/get_program_path.c
@@ -27,15 +27,10 @@ static char	*create_path(char *prefix, char *suffix)
 	return (str);
 }
 
-char	*get_path(char *cmd)
+static int	check_base(char *cmd, char **ret_path)
 {
-	char	**paths;
-	char	*path;
-	int		i;
-	struct stat path_stat;
+	struct stat	path_stat;
 
-	i = 0;
-	paths = ft_split(get_env("PATH"), ':');
 	if (ft_strchr(cmd, '/') && !access(cmd, X_OK))
 	{
 		stat(cmd, &path_stat);
@@ -44,23 +39,58 @@ char	*get_path(char *cmd)
 			print_error(cmd, "Command is a directory");
 			return (0);
 		}
-		return (ft_strdup(cmd));
+		*ret_path = ft_strdup(cmd);
+		return (1);
 	}
-	while (paths[i])
+	return (2);
+}
+
+static int	check_paths(char **paths, char *cmd, char **ret_path, char *path)
+{
+	int			i;
+	struct stat	path_stat;
+
+	i = -1;
+	while (paths[++i])
 	{
 		path = create_path(paths[i], cmd);
+		if (!path)
+		{
+			destroy_argv(paths);
+			return (0);
+		}
 		if (!access(path, X_OK))
 		{
 			stat(path, &path_stat);
 			if (!S_ISDIR(path_stat.st_mode))
 			{
 				destroy_argv(paths);
-				return (path);
+				*ret_path = path;
+				return (1);
 			}
 		}
 		free(path);
-		i++;
 	}
+	return (2);
+}
+
+char	*get_path(char *cmd)
+{
+	char		**paths;
+	char		*ret_path;
+
+	ret_path = 0;
+	if (!check_base(cmd, &ret_path))
+		return (0);
+	else if (ret_path)
+		return (ret_path);
+	paths = ft_split(get_env("PATH"), ':');
+	if (!paths)
+		return (0);
+	if (!check_paths(paths, cmd, &ret_path, 0))
+		return (0);
+	else if (ret_path)
+		return (ret_path);
 	destroy_argv(paths);
 	print_error(cmd, "Could not find command");
 	return (0);
