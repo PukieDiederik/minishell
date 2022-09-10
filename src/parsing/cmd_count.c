0991769ed6b524450dffd67e404d274795a724a9
Pukiediederik <diederik.boeren@gmail.com>
2022-09-10 19:08:00 +0100

    Norm cmd_count.c

--- src/parsing/cmd_count.c
+++ src/parsing/cmd_count.c
@@ -21,8 +21,9 @@ static int	get_return_val(int c, int has_args, const char *str)
 		return (0);
 	else if (!has_args)
 	{
-		i = ft_strlen(str);
-		while (i > 0 && str[--i] == ' ');
+		i = ft_strlen(str) - 1;
+		while (i > 0 && str[i] == ' ')
+			i--;
 		if (str[i] == '|')
 			print_error("Parsing", "Unclosed pipe at end");
 		return (-1);
@@ -30,7 +31,7 @@ static int	get_return_val(int c, int has_args, const char *str)
 	return (c);
 }
 
-int is_redirect_valid (const char *str)
+int	is_redirect_valid(const char *str)
 {
 	unsigned int	i;
 
@@ -45,9 +46,53 @@ int is_redirect_valid (const char *str)
 		if (str[++i] == '>')
 			i++;
 	}
-	while(str[i] == ' ')
+	while (str[i] == ' ')
 		i++;
 	return (str[i] == '\0' || str[i] == '<' || str[i] == '>' || str[i] == '|');
+}
+
+static int	final_checks(const char *str, int *i, int *c, int *has_args)
+{
+	if (str[*i] == '|')
+	{
+		if (!*has_args)
+		{
+			print_error("parsing", "empty pipe");
+			return (1);
+		}
+		*has_args = 0;
+		(*c)++;
+		(*i)++;
+	}
+	else
+	{
+		skip_regular(str, i);
+		*has_args = 1;
+	}
+	return (0);
+}
+
+static int	do_checks(const char *str, int *i, int *has_args, int *c)
+{
+	if ((str[*i] == '<' || str[*i] == '>'))
+	{
+		*has_args = 1;
+		if (is_redirect_valid(str + *i))
+		{
+			print_error("Parsing", "Redirect is empty");
+			return (1);
+		}
+		skip_redirect((char *)str, i);
+	}
+	else if (str[*i] == '"' || str[*i] == '\'')
+	{
+		*has_args = 1;
+		if (skip_qouted(str, i))
+			return (1);
+	}
+	else if (final_checks(str, i, c, has_args))
+		return (1);
+	return (0);
 }
 
 int	count_commands(const char *str)
@@ -61,42 +106,10 @@ int	count_commands(const char *str)
 	c = 1;
 	while (str[i])
 	{
-		if (str[i] == ' ')
-			while (str[i] == ' ')
-				i++;
-		if ((str[i] == '<' || str[i] == '>'))
-		{
-			has_args = 1;
-			if (is_redirect_valid(str + i))
-			{
-				print_error("Parsing", "Redirect is empty");
-				return (-1);
-			}
-			if (skip_redirect((char *)str, &i))
-				return (-1);
-		}
-		else if (str[i] == '"' || str[i] == '\'')
-		{
-			has_args = 1;
-			if (skip_qouted(str, &i))
-				return (-1);
-		}
-		else if (str[i] == '|')
-		{
-			if (!has_args)
-			{
-				print_error("parsing", "empty pipe");
-				return (-1);
-			}
-			has_args = 0;
-			c++;
+		while (str[i] == ' ')
 			i++;
-		}
-		else
-		{
-			skip_regular(str, &i);
-			has_args = 1;
-		}
+		if (do_checks(str, &i, &has_args, &c))
+			return (-1);
 	}
 	return (get_return_val(c, has_args, str));
 }
