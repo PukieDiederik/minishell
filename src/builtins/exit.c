544a240cee99425c391edac4c4618b1c66be38c6
Pukiediederik <diederik.boeren@gmail.com>
2022-09-22 14:50:23 +0100

    Exit now accepts an argument

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -10,19 +10,68 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
+#include "minishell.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
 
 extern char	**g_environ;
 
+static int	is_numeric(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (*str == '-' || *str == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+		i++;
+	if (str[i] != '\0'
+		|| (*str == '+' && ft_strncmp("9223372036854775807", str + 1, 20) < 0)
+		|| (*str == '-' && ft_strncmp("9223372036854775808", str + 1, 20) < 0)
+		|| (*str != '+' && ft_strncmp("9223372036854775807", str, 20) < 0))
+		return (0);
+	return (1);
+}
+
+static unsigned char	get_exit_code(const char *str)
+{
+	unsigned long long int	num;
+	int						i;
+
+	if (str == 0)
+		return (0);
+	i = 0;
+	if (!ft_isdigit(*str))
+		i++;
+	num = 0;
+	while (str[i])
+		num = (num * 10) + (str[i++] - '0');
+	if (str[0] == '-')
+		return (256 - (num & 255));
+	return (num & 255);
+}
+
 int	b_exit(t_cmd *cmdv, int i)
 {
-	if (get_argv_size(cmdv[i].argv) > 1)
+	unsigned char	exit_code;
+
+	if (cmdv[i].argv[1] != 0 && !is_numeric(cmdv[i].argv[1]))
+	{
+		print_error("exit", "numeric argument required");
+		exit_code = 2;
+	}
+	else if (get_argv_size(cmdv[i].argv) > 2)
 	{
 		print_error("exit", "Too many arguments");
-		return (2);
+		return (1);
 	}
+	else if (cmdv[i].argv[1] == 0)
+		exit_code = 0;
+	else
+		exit_code = get_exit_code(cmdv[i].argv[1]);
+	printf("exit");
 	destroy_cmdv(cmdv);
 	destroy_argv(g_environ);
-	exit(0);
+	exit(exit_code);
 }
